415649699f2d21f4f117d54cb9252119a5fd1455
suprodigy <suprodigy@gmail.com>
2017-01-12 01:01:11 +0900

    Lesson10
    Lesson10

--- Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
+++ Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
@@ -1,3 +1,4 @@
+package com.example.android.background.sync;
 /*
  * Copyright (C) 2016 The Android Open Source Project
  *
@@ -15,9 +16,23 @@
  */
 
 // TODO (9) Create WaterReminderIntentService and extend it from IntentService
-
 //  TODO (10) Create a default constructor that calls super with the name of this class
-
 //  TODO (11) Override onHandleIntent
 //      TODO (12) Get the action from the Intent that started this Service
 //      TODO (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class WaterReminderIntentService extends IntentService {
+
+    public WaterReminderIntentService() {
+        super("WaterReminderIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        String action = intent.getAction();
+        ReminderTasks.executeTask(this, action);
+    }
+}
